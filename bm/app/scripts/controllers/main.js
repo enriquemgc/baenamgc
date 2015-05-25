11f1f14c5e7f645a78d0623f4154fee00610bca1
Enrique Martínez Galván <enriquemgc@iMac-de-Enrique.local>
2015-05-25 22:52:21 +0200

    angular config change, bower_components folder removed and some routes modified

--- bm/app/scripts/controllers/main.js
+++ bm/app/scripts/controllers/main.js
@@ -1,22 +1,7 @@
 'use strict';
 
 angular.module('appApp')
-	.controller('MainController', function ($scope, $http) {
+	.controller('MainController', function ($scope) {
 
-	$scope.dbquery = function() {
-		$http.get('/query').success(function(data, status, headers, config) {
-	        $scope.items = data.nutrients;
-	        $scope.foodName = data.Long_Desc;
-	      });
-	};
-
-	$scope.dbquery();
-
-	// $scope.items = [
- //    { name: 'Pepperoni', wanted: true },
- //    { name: 'Sausage', wanted: false },
- //    { name: 'Black Olives', wanted: true },
- //    { name: 'Green Peppers', wanted: false }
- //  ];
 
   });
